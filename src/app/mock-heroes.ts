a75d0630a20fd02691f7e1e5914cffb15f0297b6
Lizaveta <elizavetashut1997@gmail.com>
2018-01-21 18:54:02 +0300

    part4

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Водяной' },
+  { id: 12, name: 'Леший' },
+  { id: 13, name: 'Кощей' },
+  { id: 14, name: 'Алёнушка' },
+  { id: 15, name: 'Колобок' },
+  { id: 16, name: 'Снегурочка' },
+  { id: 17, name: 'Илья Муромец' },
+  { id: 18, name: 'Алёша Попович' },
+  { id: 19, name: 'Садко' },
+  { id: 20, name: 'Жар-птица' }
+];
